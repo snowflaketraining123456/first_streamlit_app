0c2fe8e6a2c4785fd13889010f422af4f56be2de
snowflaketraining123456 <127636535+snowflaketraining123456@users.noreply.github.com>
2023-03-12 11:52:19 +1100

    Update streamlit_app.py
    Added new header

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamtlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
