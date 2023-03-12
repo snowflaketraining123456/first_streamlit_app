3d3328e8a8d27b8b36a26d9bbdd43da081ecfcf0
snowflaketraining123456 <127636535+snowflaketraining123456@users.noreply.github.com>
2023-03-12 11:52:53 +1100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,4 +8,4 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
-streamtlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.text('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
